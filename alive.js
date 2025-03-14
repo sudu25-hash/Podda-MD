8784a35201e2ad27573b080fbaa66734cc07a017
yyyt5 <yrandula0@gmail.com>
2025-03-14 11:51:46 +0530

    Create alive.js

---
+++ alive.js
@@ -0,0 +1,53 @@
+const { cmd, commands } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+
+cmd({
+    pattern: "alive",
+    alias: ["status", "runtime", "uptime"],
+    desc: "Check uptime and system status",
+    category: "main",
+    react: "📟",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        // Generate system status message
+        const status = `╭━━〔 *ASHUU-MD-V1* 〕━━┈⊷
+┃◈╭─────────────·๏
+┃◈┃• *⏳Uptime*:  ${runtime(process.uptime())} 
+┃◈┃• *📟 Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
+┃◈┃• *⚙️ HostName*: ${os.hostname()}
+┃◈┃• *👨‍💻 Owner*: ᴄʏʙᴇʀ ʟᴏᴋᴜ ᴀꜱʜᴜᴜ
+┃◈┃• *🧬 Version*: V1 BETA
+┃◈└───────────┈⊷
+╰──────────────┈⊷
+
+  𝐡𝐞𝐥𝐥𝐨𝐰 𝐢𝐦 𝐚𝐬𝐡𝐮𝐮 𝐦𝐝 𝐯1 𝐛𝐨𝐭.𝐢𝐦 𝐚𝐥𝐢𝐯𝐞 𝐧𝐨𝐰. 
+
+  https://whatsapp.com/channel/0029Vb4HJcD4inowFiwWsY1S
+
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʏʙᴇʀ ʟᴏᴋᴜ ᴀꜱʜᴜᴜ`;
+
+        // Send the status message with an image
+        await conn.sendMessage(from, { 
+            image: { url: `https://i.postimg.cc/xdMvP3XZ/In-Shot-20241222-002123636.jpg` },  // Image URL
+            caption: status,
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363357105376275@g.us@newsletter',
+                    newsletterName: 'ᴍʀ ᴅɪɴᴇꜱʜ',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.error("Error in alive command:", e);
+        reply(`An error occurred: ${e.message}`);
+    }
+});
+                      
