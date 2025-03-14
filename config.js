8fe69036d9184895cefde35394f45ea51534666e
yyyt5 <yrandula0@gmail.com>
2025-03-14 13:32:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "vQJXlYTS#n4_LCry2tR8U2T8ujaDqZ-9G8PCY9pFlf0QfUbDRyCg",
+    SESSION_ID: process.env.SESSION_ID || "PODDA-MD=vQJXlYTS#n4_LCry2tR8U2T8ujaDqZ-9G8PCY9pFlf0QfUbDRyCg",
     CAPTION: process.env.CAPTION || "POWERED BY CYBER LOKU ASHUU",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
